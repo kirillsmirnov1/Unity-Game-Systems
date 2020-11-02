209a4c78eb936db41bc7c0bf875a52c37c84f5ea
Kirill Smirnov <kirill@dg-line.ru>
2020-11-02 17:43:05 +0300

    UGS: BaseSaveManager.cs and BaseBalanceManager.cs

---
+++ Runtime/Persistent/BaseBalanceManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityGameSystems.Persistent
+{
+    public abstract class BaseBalanceManager : MonoBehaviour
+    {
+        public static event Action<int> OnBalanceChange;
+
+        private int _balance;
+
+        public int Balance
+        {
+            get => _balance;
+            protected set
+            {
+                if(_balance == value) return;
+                _balance = value;
+                OnBalanceChange?.Invoke(_balance);
+            }
+        }
+
+        protected virtual void Awake() => BindCallbacks();
+        protected virtual void OnDestroy() => UnBindCallbacks();
+
+        protected abstract void BindCallbacks();
+        protected abstract void UnBindCallbacks();
+
+        public void ChangeBalance(int value) => Balance += value;
+    }
+}

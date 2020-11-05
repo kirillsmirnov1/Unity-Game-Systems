83f76d18ad98b33ca9d6fda55528d780a520d98b
Kirill Smirnov <kirill@dg-line.ru>
2020-11-05 11:15:44 +0300

    UGS v1.1.0 • BalanceDisplay.cs

---
+++ Runtime/Display/BalanceDisplay.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityGameSystems.Persistent;
+using Zenject;
+
+namespace UnityGameSystems.Display
+{
+    public class BalanceDisplay : MonoBehaviour
+    {
+#pragma warning disable 0649
+        [Inject] private BaseBalanceManager _balanceManager;
+#pragma warning restore 0649
+        
+        private TextMeshProUGUI _text;
+
+        private void Awake()
+        {
+            _text = GetComponentInChildren<TextMeshProUGUI>();
+            BaseBalanceManager.OnBalanceChange += SetText;
+        }
+
+        private void OnDestroy()
+        {
+            BaseBalanceManager.OnBalanceChange -= SetText;
+        }
+
+        private void Start() => SetText(_balanceManager.Balance);
+
+        private void SetText(int balance) => _text.text = balance.ToString();
+    }
+}
